2dabc6f416bcf552c92d5e4ad6029076d2ff1808
MichelaMilzoni <michelamilzoni0@gmail.com>
2025-04-02 14:43:30 +0200

    STAMPA NUMERI DA 1 A 100

---
+++ main.js
@@ -0,0 +1,24 @@
+// Scrivi un programma che stampi i numeri da 1 a 100,
+
+// RACCOLTA DATI
+
+// - per i multipli di 3 stampi "Fizz" al posto del numero
+const multipleOf3 = `Fizz`
+// - per i multipli di 5 stampi "Buzz" al posto del numero
+const multipleOf5 = `Buzz`
+// - per i multipli di 3 e di 5 stampi "FizzBuzz" al posto del numero
+const multipleOf3And5 = `FizzBuzz`
+
+let outputText;
+
+// Prima di partire a scrivere codice poniamoci qualche domanda:
+// Come faccio a sapere se un numero è divisibile per un altro? Abbiamo visto qualcosa di particolare
+// che possiamo usare?
+
+// SVOLGIMENTO
+for (let i = 1; i <= 100; i++) {
+    console.log(i);
+    
+}
+
+// OUTPUT
